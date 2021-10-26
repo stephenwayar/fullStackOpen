42d8ea6aa3118dcfbe6bdfb12d82f38108931421
Stephen Wayar <stephenbuluswayar@gmail.com>
2021-10-26 20:22:46 +0100

    redid a small exercise

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -86,41 +86,69 @@ import React, { useState } from "react";
 //   )
 // };
 
+
+// const App = () => {
+
+//   const anecdotes = [
+
+//     'If it hurts, do it more often',
+
+//     'Adding manpower to a late software project makes it later!',
+
+//     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+
+//     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+
+//     'Premature optimization is the root of all evil.',
+
+//     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+
+//     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+//   ];
+
+//   const [anecdote, setAnecdote] = useState(0);
+
+//   const handleClick = () => {
+//     const randomNum = Math.floor(Math.random() * (7 - 0) + 0);
+//     setAnecdote(randomNum);
+//   };
+
+//   console.log(anecdote)
+
+//   return (
+//     <div>
+
+//       <p>{anecdotes[anecdote]}</p>
+//       <button>Vote</button>
+//       <button onClick={handleClick}>Next</button>
+
+//     </div>
+//   )
+// }
+
 const App = () => {
 
-  const anecdotes = [
+  const [clicks, setClicks] = useState({left: 0, right: 0});
 
-    'If it hurts, do it more often',
+  const [allClicks, setAllClicks] = useState([]);
 
-    'Adding manpower to a late software project makes it later!',
-
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-
-    'Premature optimization is the root of all evil.',
-
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ];
-
-  const [anecdote, setAnecdote] = useState(0);
-
-  const handleClick = () => {
-    const randomNum = Math.floor(Math.random() * (7 - 0) + 0);
-    setAnecdote(randomNum);
+  const handleLeftClick = () => {
+    setClicks({...clicks, left: clicks.left + 1});
+    setAllClicks(allClicks.concat('L'));
   };
 
-  console.log(anecdote)
+  const handleRightClick =() => {
+    setClicks({...clicks, right: clicks.right + 1});
+    setAllClicks(allClicks.concat('R'));
+  };
 
   return (
     <div>
-
-      <p>{anecdotes[anecdote]}</p>
-      <button>Vote</button>
-      <button onClick={handleClick}>Next</button>
-
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
