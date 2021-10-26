de13e4c4a8b1547e57e6b64f0e9d95d0d5f06399
Stephen Wayar <stephenbuluswayar@gmail.com>
2021-10-27 00:32:43 +0100

    made progress with the map()

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -87,70 +87,51 @@ import React, { useState } from "react";
 // };
 
 
-// const App = () => {
-
-//   const anecdotes = [
-
-//     'If it hurts, do it more often',
-
-//     'Adding manpower to a late software project makes it later!',
-
-//     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-
-//     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-
-//     'Premature optimization is the root of all evil.',
-
-//     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-
-//     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-//   ];
-
-//   const [anecdote, setAnecdote] = useState(0);
-
-//   const handleClick = () => {
-//     const randomNum = Math.floor(Math.random() * (7 - 0) + 0);
-//     setAnecdote(randomNum);
-//   };
-
-//   console.log(anecdote)
-
-//   return (
-//     <div>
-
-//       <p>{anecdotes[anecdote]}</p>
-//       <button>Vote</button>
-//       <button onClick={handleClick}>Next</button>
-
-//     </div>
-//   )
-// }
-
 const App = () => {
 
-  const [clicks, setClicks] = useState({left: 0, right: 0});
+  const anecdotes = [
 
-  const [allClicks, setAllClicks] = useState([]);
+    'If it hurts, do it more often',
 
-  const handleLeftClick = () => {
-    setClicks({...clicks, left: clicks.left + 1});
-    setAllClicks(allClicks.concat('L'));
+    'Adding manpower to a late software project makes it later!',
+
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+
+    'Premature optimization is the root of all evil.',
+
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ];
+
+  const presentAnecdote = () => Math.floor(Math.random() * anecdotes.length);
+  const [selected, setSelected] = useState(presentAnecdote);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
+  const buttonHandler = () => {
+    setSelected(presentAnecdote);
   };
 
-  const handleRightClick =() => {
-    setClicks({...clicks, right: clicks.right + 1});
-    setAllClicks(allClicks.concat('R'));
+  const vote = () => {
+    const nvotes = [...votes];
+    nvotes[selected] += 1;
+    setVotes(nvotes);
   };
+  const highestVote = Math.max(...votes);
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
-      <p>{allClicks.join(' ')}</p>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={vote}>vote</button>
+      <button onClick={buttonHandler}>next anecdote</button>
+      <h1>Anecdotes with the most votes</h1>
+      <p>{anecdotes[votes.indexOf(highestVote)]}</p>
+      <p>has {highestVote} votes</p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
