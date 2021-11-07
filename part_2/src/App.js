83567c16c69e409cc9ac42923b94e6b1d7f75781
stephenwayar <stephenbuluswayar@gmail.com>
2021-11-07 19:56:42 +0100

    new method of handling forms with state hook

--- part_2/src/App.js
+++ part_2/src/App.js
@@ -1,56 +1,44 @@
 import React, {useState} from "react";
-import Note from "./components/note.component";
+// import Note from "./components/note.component";
 
+const App = ({ notes }) => {
 
-const App = (props) => {
+  const [name, setname ] =  useState("")
 
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('') 
-  const [showAll, setShowAll] = useState(true)
-  
-  const addNote = (event) => {
-
-    event.preventDefault()
-
-    const noteObject = {
-      id: notes.length + 1,
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5
-    }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-
-    console.log('button clicked', event.target.value)
+  const handleChange = (e) => {
+    setname(e.target.value)
   }
 
-  const handleNewNote = (event) => {
-    setNewNote(event.target.value)
-    console.log(event.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`This name you entered is ${name}`)
   }
 
-  console.log(notes);
+  console.log(name);
 
- const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+  if (name === "") {
+    return(
+      <div>
+        <input type="text" value={name} onChange={handleChange}/>
+        <button>submit</button>
+      </div>
+    )
+  }
 
   return (
     <div>
-
-      <h1>Notes</h1>
-
+      {/* <h1>Notes</h1>
       <ul>
-        {props.notes.map(note => 
+        {notes.map(note => 
             <Note key={note.id} note={note} />
         )}
-      </ul>
-
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNewNote}/>
-        <button type="submit"> Save </button>
-      </form>
-
+      </ul> */}
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name here:</label>
+      <input type="text" value={name} onChange={handleChange}/>
+      <button>submit</button>
+    </form>
+    <p>Hello {name}</p>
     </div>
   )
 }
