84d66147139dbebae3b924604a036d71c8c0aa66
stephenwayar <stephenbuluswayar@gmail.com>
2021-11-25 22:29:03 +0100

    The phonebook exercise 2.8 completed

--- part_2/src/App.js
+++ part_2/src/App.js
@@ -1,37 +1,52 @@
 import React from "react";
 import { useState } from "react/cjs/react.development";
 
-const Person = ({name}) => {
+const Person = ({name, number}) => {
   return(
-    <p>{name}</p>
+    <p>
+      {name === "" ? "" : <b>Name:</b>} {name} 
+      {number === "" ? "" : <b>Number:</b>} {number}
+    </p>
   )
 }
-
+ 
 const App = () => {
 
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: 123456789 }
   ]) 
 
   const [ newName, setNewName ] = useState('')
+  const [newNumber, setNewNumber] = useState("")
 
-  const handleChange = e => {
+  const handleNameChange = e => {
     setNewName(e.target.value)
     console.log(e.target.value);
   }
 
-  const found = persons.find(person => (person.name === newName))
+  const handleNumberChange = ev => {
+    setNewNumber(ev.target.value)
+    console.log(ev.target.valuel);
+  }
+
+  const nameFound = persons.find(person => (person.name === newName))
 
   const handleSubmit = e => {
     e.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    if(found){
+    if(newName === ""){
+      alert("Input cannt be empty")
+    }
+    else if(nameFound){
       alert(`${newName} already exists`)
-    }else{
+    }
+    else{
       setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNumber("")
     }
   }
 
@@ -40,14 +55,16 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input type="text" value={newName} onChange={handleNameChange}/>
+          <br />
+          number: <input type="number" value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       
-      {persons.map((person, index) => (<Person key={index} name={person.name}/>))}
+      {persons.map((person, index) => (<Person key={index} name={person.name} number={person.number}/>))}
     </div>
   )
 }
